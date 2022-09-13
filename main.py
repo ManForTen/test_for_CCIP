92b3f271ba5cf2abeae3fac4461e628a9fb17842
Artem Petrikov <manforten@yandex.ru>
2022-09-13 14:39:34 +0300

    add new F3

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ a = int(input("First number:"))
 b = int(input("Second number:"))
 F = a + b
 F2 = a - b
+F3 = a * b
 print('Your result(+)',F)
 print('Your result(-)',F2)
+print('Your result(*)',F3)
